5a2353ae870da4d11c24ac091bccc4deec0093c6
aleksey <lil_pop96@bk.ru>
2022-05-23 01:56:36 +0700

    init

---
+++ Assets/Src/Krokofol/Application/Model/Wall/WallTile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Src.Krokofol.Application.Model.Wall
+{
+    public class WallTile : MonoBehaviour
+    {
+        public Tilemap tilemap = new Tilemap();
+
+        public void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Src/Krokofol/Application/Model/Wall/WallType.cs
@@ -0,0 +1,52 @@
+﻿namespace Src.Krokofol.Application.Model.Wall
+{
+    public enum WallType
+    {
+        RightFullDownLeft, //0
+        RightFullDownFullLeft, //1
+        UpRightDown, //2
+        UpFullRightFullDown, //3
+        UpRightFullDown, //4
+        UpFullRightDown, //5
+        DownLeftUp, //6
+        DownFullLeftFullUp, //7
+        DownFullLeftUp, //8
+        DownLeftFullUp, //9
+        Full, //10
+        Single, //11
+        Up, //12
+        UpDown, //13
+        Down, //14
+        Right, //15
+        UpFullRight, //16
+        UpRight, //17
+        Left, //18
+        LeftFullUp, //19
+        LeftUp, //20
+        LeftUpRight, //21
+        LeftFullUpRight, //22
+        LeftUpFullRight, //23
+        LeftFullUpFullRight, //24
+        DownLeft, //25
+        DownFullLeft, //26
+        RightDown, //27
+        RightFullDown, //28
+        RightDownLeft, //29
+        RightDownFullLeft, //30
+        UpRightDownLeft, //31
+        UpFullRightDownLeft, //32
+        UpRightDownLeftFull, //33
+        UpRightFullDownLeft, //34
+        UpRightDownFullLeft, //35
+        UpFullRightDownLeftFull, //36
+        UpRightFullDownLeftFull, //37
+        UpRightDownFullLeftFull, //38
+        UpFullRightFullDownLeft, //39
+        UpFullRightDownFullLeft, //40
+        UpRightFullDownFullLeft, //41
+        UpFullRightFullDownLeftFull, //44
+        UpFullRightDownFullLeftFull, //45
+        UpRightFullDownFullLeftFull, //46
+        UpFullRightFullDownFullLeft //47
+    }
+}
